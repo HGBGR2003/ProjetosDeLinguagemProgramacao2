af7d523941746a08e3ddba1ad47ca906bd907d2d
Henrique Gabriel Barbosa <guidarodrigues2003@gmail.com>
2026-08-08 11:01:51 -0300

    Passo 1 - Terminado

--- ProjetoMedicao/src/br/edu/ifgoiano/edu/alunos/Main.java
+++ ProjetoMedicao/src/br/edu/ifgoiano/edu/alunos/Main.java
@@ -1,7 +1,23 @@
 package br.edu.ifgoiano.edu.alunos;
 
 public class Main {
-    static void main(String[] args) {
+   public static void main(String[] args) {
+        Medidor medidor = new Medidor("00:1A:2B:3C:4D:4E","bifásico");
+
+        medidor.registrarConsumo(10.5);
+        //medidor.registrarConsumo(-3); //Irá dar erro, pois o consumo não pode ser negativo!
+
+        medidor.setTensao(0,220);
+        medidor.setTensao(0,210);
+        //medidor.setTensao(2,290); // Passou do limite que está entre 90V á 250V.
+
+        medidor.registrarInjecao(3.2);
+
+        Consumidor consumidor = new Consumidor("Henrique Gabriel Barbosa Guida", "6627595724", medidor);
+
+        System.out.println("Consumidor: " + consumidor.getNome());
+        System.out.println("MAC: " + medidor.getEnderecoMac());
+        System.out.println("Consumo: " + medidor.getValorConsumido() + "kwh");
 
     }
 }
